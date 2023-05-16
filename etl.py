2fc2a0ed4faf7b4556f08915bac8eb60ce83be45
yasarsultan <yasersultan03@gmail.com>
2023-05-17 02:01:36 +0530

    resources initialized

---
+++ etl.py
@@ -0,0 +1,11 @@
+# Remember to activate environment before running this script.
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+import datetime as datetime
+
+# tmpfile = "temp.tmp"            #file to store all extracted data
+# logfile = "logfile.txt"         #file to store all log events
+# targetfile = "transformed.csv"  #file to store transformed data
+
+print("The above imports are successfully installed")
