7b9cfaa683984b3609f9ce69194e07be0d24272e
yasarsultan <yasersultan03@gmail.com>
2023-05-18 19:50:14 +0530

    performed basic etl job

--- etl.py
+++ etl.py
@@ -2,10 +2,93 @@
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
-import datetime as datetime
+from datetime import datetime
 
-# tmpfile = "temp.tmp"            #file to store all extracted data
-# logfile = "logfile.txt"         #file to store all log events
-# targetfile = "transformed.csv"  #file to store transformed data
+#tmpfile = "temp.tmp"            #file to store all extracted data
+logfile = "logfile.txt"         #file to store all log events
+targetfile = "transformed.csv"  #file to store transformed data
 
-print("The above imports are successfully installed")
+# data extracting function for csv file
+def extract_csv(file):
+    df = pd.read_csv(file)
+    return df
+
+# for json file
+def extract_json(file):
+    df = pd.read_json(file,lines=True)
+    return df
+
+# for xml file
+def extract_xml(file):
+    df = pd.DataFrame(columns=['name', 'height', 'weight'])
+    tree = ET.parse(file)
+    root = tree.getroot()
+    for person in root:
+        name = person.find("name").text
+        height = float(person.find('height').text)
+        weight = float(person.find('weight').text)
+        df = df._append({"name":name, "height":height, "weight":weight}, ignore_index = True)
+    return df
+
+# Extracting all the files 
+def extract():
+    extracted_data = pd.DataFrame(columns=['name', 'height', 'weight'])
+
+    for csv in glob.glob('source/*.csv'):
+        extracted_data = extracted_data._append(extract_csv(csv), ignore_index = True)
+    
+    for json in glob.glob('source/*.json'):
+
+        extracted_data = extracted_data._append(extract_json(json), ignore_index = True)
+
+    for xml in glob.glob('source/*.xml'):
+        extracted_data = extracted_data._append(extract_xml(xml), ignore_index = True)
+    
+    return extracted_data
+
+# # check
+# data = extract()
+# print(data)
+
+def transform(dataset):
+    # Converting inches to meters *one inch=0.0254 meter
+    dataset['height']= round(dataset.height * 0.0254, 2)
+
+    # converting pounds to kilograms 1 pound = 0.45359237 kg
+    dataset['weight']= round(dataset.weight * 0.45359237, 2)
+
+    return dataset
+
+## check
+# res = transform(data)
+# print(res)
+
+# Load all the data into a desired file
+def load(target_file, data):
+    data.to_csv(target_file)
+
+# Log messages
+def log(msg):
+    timestamp_format = "%d-%h-%Y  %H-%M"
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open("logfile.txt", "a") as file:
+        file.write(timestamp + ", " + msg + '\n')
+
+
+
+log("ETL job started")
+
+log("Extract phase started")
+extracted = extract()
+log("Extract phase ended")
+
+log("Transform phase started")
+transformed = transform(extracted)
+log("Transform phase ended")
+
+log("Load phase started")
+load(targetfile, transformed)
+log("Load phase ended")
+
+log("ETL job ended")
